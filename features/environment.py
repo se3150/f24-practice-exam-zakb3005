e2fe5e63f25107f8555436f42896d4558751c415
zakb3005 <zakb3005@gmail.com>
2024-12-08 17:01:36 -0700

    trying to fix environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,8 @@
 import behave_webdriver
+from behave_webdriver.steps import *
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+    context.behave_driver = behave_webdriver.Chrome()  # Use `context.behave_driver`
 
 def after_all(context):
     context.behave_driver.quit()
