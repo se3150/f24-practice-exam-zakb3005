1791967f9b83da1e85ea69e9bf3537cfb2997e05
zakb3005 <zakb3005@gmail.com>
2024-12-08 17:19:10 -0700

    please work

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,15 @@
 import behave_webdriver
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome.headless()
-    #context.behave_driver = behave_webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    context.behave_driver = behave_webdriver.Chrome(options=chrome_options)
 
 def after_all(context):
-    context.behave_driver.quit()
+    if hasattr(context, 'behave_driver'):
+        context.behave_driver.quit()
