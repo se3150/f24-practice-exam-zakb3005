5a2323d2cf3516a387f746b699f0c03df83e1f9d
zakb3005 <zakb3005@gmail.com>
2024-12-08 17:10:48 -0700

    work

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,8 @@
 import behave_webdriver
-from behave_webdriver.steps import *
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()  # Use `context.behave_driver`
+    #context.behave_driver = behave_webdriver.Chrome.headless()
+    context.behave_driver = behave_webdriver.Chrome()
 
 def after_all(context):
     context.behave_driver.quit()
